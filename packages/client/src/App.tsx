03a226a29849be405057d772aa583116e7c111b3
ahmedthousif571 <ahmedthousif9388@gmail.com>
2026-02-10 17:09:19 +0530

    husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,15 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+import { Button } from './components/ui/button';
 
-function App(){
-  const [message,setMessage] = useState('')
+function App() {
+   const [message, setMessage] = useState('');
 
-  useEffect(()=>{
-    fetch('/api/hello')
-    .then(res=> res.json())
-    .then(data => setMessage(data.message))
-  },[])
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message));
+   }, []);
 
-  return <p className="font-bold">{message}</p>
+   return (
+      <div className="p-4">
+         <p className="font-bold">{message}</p>
+         <Button>Click me </Button>
+      </div>
+   );
 }
 
-export default App
+export default App;
